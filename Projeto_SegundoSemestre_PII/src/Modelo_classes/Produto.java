3c6f5a37fdc17709d3d75bc8a183a5687b2beefb
nails <nails@DESKTOP-BTAHTQN>
2020-04-10 11:57:41 -0300

    adequação do layout - 10/04/2020 	ajustes para deixar ele semântico

--- Projeto_SegundoSemestre_PII/src/Modelo_classes/Produto.java
+++ Projeto_SegundoSemestre_PII/src/Modelo_classes/Produto.java
@@ -11,16 +11,66 @@ package Modelo_classes;
  */
 public class Produto {
     
-    public String nomeDescricao;
-    public String CodigoFabricante;
-    public String marca;
-    public String[] tipo;
-    public int quantidade;
+    private String descricao;
+    private String codigoFabricante;
+    private String marca;
+    private String[] tipo;
+    private int quantidade;
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getCodigoFabricante() {
+        return codigoFabricante;
+    }
+
+    public void setCodigoFabricante(String codigoFabricante) {
+        this.codigoFabricante = codigoFabricante;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String[] getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String[] tipo) {
+        this.tipo = tipo;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
     public String toString(){
         return(
-                "Nome: "+ nomeDescricao+
-                "codigo: "+CodigoFabricante+
+                "Nome: "+ descricao+
+                "codigo: "+ codigoFabricante+
                 "Marca: "+ marca+
                 "tipo: "+ tipo[0] +
                 "Quantiade em Estoque: " + quantidade);
