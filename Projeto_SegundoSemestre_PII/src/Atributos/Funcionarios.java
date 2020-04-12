0b1d4a40b4822ec9231b287d9fb1d65e53e39d8f
Marcos Vinicius Santos Souza <>
2020-04-12 13:15:57 -0300

    Desenvolvido getters and setters

--- Projeto_SegundoSemestre_PII/src/Atributos/Funcionarios.java
+++ Projeto_SegundoSemestre_PII/src/Atributos/Funcionarios.java
@@ -15,14 +15,123 @@ import java.text.SimpleDateFormat;
 public class Funcionarios {
     
     private int id;
+    private int cep;
+    private int numeroCasa;
     private String nome;
-    private DateFormat formataData = new SimpleDateFormat("yyyy-MM-dd");
+    private DateFormat data = new SimpleDateFormat("dd-MM-yyyy");
     private String cpf;
     private String cargo;
     private String rua;
-    private String cep;
-    private int numeroCasa;
     private String bairro;
     private String email;
     private String telefone;
+    private String senha1;
+    private String senha2;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public int getCep() {
+        return cep;
+    }
+
+    public void setCep(int cep) {
+        this.cep = cep;
+    }
+
+    public int getNumeroCasa() {
+        return numeroCasa;
+    }
+
+    public void setNumeroCasa(int numeroCasa) {
+        this.numeroCasa = numeroCasa;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getSenha1() {
+        return senha1;
+    }
+
+    public void setSenha1(String senha1) {
+        this.senha1 = senha1;
+    }
+
+    public String getSenha2() {
+        return senha2;
+    }
+
+    public void setSenha2(String senha2) {
+        this.senha2 = senha2;
+    }
+
+    public DateFormat getData() {
+        return data;
+    }
+
+    public void setData(DateFormat data) {
+        this.data = data;
+    }
+    
+    
+    
 }
