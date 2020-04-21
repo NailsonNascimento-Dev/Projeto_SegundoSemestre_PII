892001f3e17073f54eff8a3c8ad1854d5da4cf5f
Diegossilva01 <61715729+Diegossilva01@users.noreply.github.com>
2020-04-21 14:24:29 -0300

    tela login ok

--- Projeto_SegundoSemestre_PII/src/Projeto_Views/TelaLogin.java
+++ Projeto_SegundoSemestre_PII/src/Projeto_Views/TelaLogin.java
@@ -5,7 +5,11 @@
  */
 package Projeto_Views;
 
+//import Projeto_Views.telaAdministrador;
+//import Projeto_Views.telaVendedor;
+//import javax.swing.JOptionPane;
 import javax.swing.JOptionPane;
+import sun.security.util.Password;
 
 /**
  *
@@ -30,41 +34,29 @@ public class TelaLogin extends javax.swing.JFrame {
     private void initComponents() {
 
         jPanel1 = new javax.swing.JPanel();
-        jLabel1 = new javax.swing.JLabel();
-        CampoUsuario = new javax.swing.JTextField();
         jLabel2 = new javax.swing.JLabel();
-        CampoSenha = new javax.swing.JPasswordField();
+        Usuario = new javax.swing.JTextField();
+        Senha = new javax.swing.JPasswordField();
         jButton1 = new javax.swing.JButton();
         jButton2 = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Tela Login");
+        setBackground(new java.awt.Color(255, 255, 255));
+        setResizable(false);
 
-        jPanel1.setBackground(new java.awt.Color(255, 255, 255));
+        jPanel1.setBackground(new java.awt.Color(222, 222, 222));
         jPanel1.setLayout(null);
 
-        jLabel1.setText("Usuário");
-        jPanel1.add(jLabel1);
-        jLabel1.setBounds(150, 190, 70, 16);
-
-        CampoUsuario.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                CampoUsuarioActionPerformed(evt);
-            }
-        });
-        jPanel1.add(CampoUsuario);
-        CampoUsuario.setBounds(220, 180, 250, 30);
-
-        jLabel2.setText("Senha");
+        jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagens/TELLLAA.jpg"))); // NOI18N
         jPanel1.add(jLabel2);
-        jLabel2.setBounds(150, 230, 40, 16);
-
-        CampoSenha.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                CampoSenhaActionPerformed(evt);
-            }
-        });
-        jPanel1.add(CampoSenha);
-        CampoSenha.setBounds(220, 220, 250, 30);
+        jLabel2.setBounds(70, 30, 360, 130);
+        jPanel1.add(Usuario);
+        Usuario.setBounds(180, 190, 250, 30);
+        jPanel1.add(Senha);
+        Senha.setBounds(180, 240, 250, 30);
 
         jButton1.setText("Entrar");
         jButton1.addActionListener(new java.awt.event.ActionListener() {
@@ -73,60 +65,81 @@ public class TelaLogin extends javax.swing.JFrame {
             }
         });
         jPanel1.add(jButton1);
-        jButton1.setBounds(330, 260, 73, 32);
+        jButton1.setBounds(270, 290, 80, 25);
 
-        jButton2.setText("Sair");
+        jButton2.setText("sair");
         jButton2.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton2ActionPerformed(evt);
             }
         });
         jPanel1.add(jButton2);
-        jButton2.setBounds(410, 260, 60, 32);
+        jButton2.setBounds(360, 290, 70, 25);
+
+        jLabel1.setText("Usuario");
+        jPanel1.add(jLabel1);
+        jLabel1.setBounds(110, 190, 60, 15);
+
+        jLabel3.setText("Senha");
+        jPanel1.add(jLabel3);
+        jLabel3.setBounds(110, 240, 40, 20);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 519, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 350, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
-        setSize(new java.awt.Dimension(535, 389));
+        setSize(new java.awt.Dimension(549, 439));
         setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
-    private void CampoUsuarioActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_CampoUsuarioActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_CampoUsuarioActionPerformed
-
-    private void CampoSenhaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_CampoSenhaActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_CampoSenhaActionPerformed
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+       System.exit(0);
+    }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-           if(CampoUsuario.getText().equals("admin")&& CampoSenha.getText().equals("1234")){
+        
+        
+        String pass = String.valueOf( Senha.getPassword() );
+        
+        if (Usuario.getText().equals("")|| (pass.equals(""))){
             
-           // JOptionPane.showConfirmDialog(null, " senha correta !!!");
-           
-         
-           telaAdministrador tela = new telaAdministrador();
-           tela.setVisible(true);
-            dispose();
-            
+           JOptionPane.showMessageDialog(this,"Campos usuario e senha invalidos");
         }
         
-       else {
-           JOptionPane.showMessageDialog(null, "senha incorreta !!!");
-               }
+        else  if(Usuario.getText().equals("admin")&& (pass.equals("1234"))){
+               
+            telaAdministrador telaAdmin = new telaAdministrador();
+             telaAdmin.setVisible(true);
+              dispose();
+               
+           }
+            
+          else if(Usuario.getText().equals("funcionario")&& (pass.equals("1234"))){
+               
+            telaVendedor telavendedor = new telaVendedor();
+             telavendedor.setVisible(true);
+             dispose();
+               
+           }
+           else{
+                JOptionPane.showMessageDialog(null, "senha  incorreta!!");
+           }
+               
+      
+      
+        
+        
+        
+        
+        
     }//GEN-LAST:event_jButton1ActionPerformed
-
-    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-        System.exit(0); 
-    }//GEN-LAST:event_jButton2ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -164,12 +177,13 @@ public class TelaLogin extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JPasswordField CampoSenha;
-    private javax.swing.JTextField CampoUsuario;
+    private javax.swing.JPasswordField Senha;
+    private javax.swing.JTextField Usuario;
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
     private javax.swing.JPanel jPanel1;
     // End of variables declaration//GEN-END:variables
 }
