a8611d7af55b7164f82cb4451624316a3052559a
Marcos Vinicius Santos Souza <Azazel@DESKTOP-TPQETM4>
2020-05-12 11:51:56 -0300

    Estava dando erro na herança por algum motivo então removi a classe pessoa como pai porém tentarei rever isso mais para frente.

--- Projeto_SegundoSemestre_PII/src/com/loja/informatica/MODEL/Funcionarios.java
+++ Projeto_SegundoSemestre_PII/src/com/loja/informatica/MODEL/Funcionarios.java
@@ -10,11 +10,25 @@ package com.loja.informatica.MODEL;
  *
  * @author Azazel
  */
-public class Funcionarios extends Pessoa{
+public class Funcionarios{
     
     private String cargo;
     private String senha1;
     private String senha2;
+    
+    
+    private int id;
+    private String nome;
+    private String data;
+    private String cpf;
+    private String sexo;
+    private String rua;
+    private String cep;
+    private int numeroCasa;
+    private String bairro;
+    private String email;
+    private String telefone;
+    
 
     public String getCargo() {
         return cargo;
@@ -39,5 +53,106 @@ public class Funcionarios extends Pessoa{
     public void setSenha2(String senha2) {
         this.senha2 = senha2;
     }
+    
+    
+    
+ 
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public int getNumeroCasa() {
+        return numeroCasa;
+    }
+
+    public void setNumeroCasa(int numeroCasa) {
+        this.numeroCasa = numeroCasa;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
 
 }
