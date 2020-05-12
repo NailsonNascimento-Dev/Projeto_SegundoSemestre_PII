d8d136f25bf44c85626914f4f4548b1e40b9d395
Marcos Vinicius Santos Souza <Azazel@DESKTOP-TPQETM4>
2020-05-12 11:44:57 -0300

    Metodo para inserir na camada DAO dos funcionarios finalizado

--- Projeto_SegundoSemestre_PII/src/com/loja/informatica/DAO/FuncionariosDAO.java
+++ Projeto_SegundoSemestre_PII/src/com/loja/informatica/DAO/FuncionariosDAO.java
@@ -5,10 +5,68 @@
  */
 package com.loja.informatica.DAO;
 
+import com.loja.informatica.MODEL.Funcionarios;
+import com.loja.informatica.UTILS.ConexaoMysql;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 /**
  *
  * @author Azazel
  */
 public class FuncionariosDAO {
-    
+
+    public static boolean cadastrarFuncionario(Funcionarios funcionarios) {
+        boolean retorno = false;
+        Connection conexao = null;
+        PreparedStatement instrucaoSQL = null;
+
+        try {
+
+            conexao = ConexaoMysql.abrirConexao();
+
+            instrucaoSQL = conexao.prepareStatement("INSERT INTO loja (nome, sexo, data_nascimento, cpf, cargo, rua, cep, numero_casa, bairro, email, telefone, senha) VALUES (?, ?, ?, ?, ?, ?, ?, ? , ?, ?, ?, ?)");
+                    
+
+            instrucaoSQL.setString(1, funcionarios.getNome());
+            instrucaoSQL.setString(2, funcionarios.getSexo());
+            instrucaoSQL.setString(3, funcionarios.getData());
+            instrucaoSQL.setString(4, funcionarios.getCpf());
+            instrucaoSQL.setString(5, funcionarios.getCargo());
+            instrucaoSQL.setString(6, funcionarios.getRua());
+            instrucaoSQL.setString(7, funcionarios.getCep());
+            instrucaoSQL.setInt(8, funcionarios.getNumeroCasa());
+            instrucaoSQL.setString(9, funcionarios.getBairro());
+            instrucaoSQL.setString(10, funcionarios.getEmail());
+            instrucaoSQL.setString(11, funcionarios.getTelefone());
+            instrucaoSQL.setString(12, funcionarios.getSenha1());
+
+            int linhasAfetadas = instrucaoSQL.executeUpdate();
+
+            if (linhasAfetadas >= 1) {
+                retorno = true;
+            } else {
+                retorno = false;
+            }
+
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        } finally {
+
+            try {
+                if (instrucaoSQL != null) {
+                    instrucaoSQL.close();
+                }
+
+                ConexaoMysql.fecharConexao();
+
+            } catch (SQLException ex) {
+            }
+        }
+
+        return retorno;
+
+    }
+
 }
