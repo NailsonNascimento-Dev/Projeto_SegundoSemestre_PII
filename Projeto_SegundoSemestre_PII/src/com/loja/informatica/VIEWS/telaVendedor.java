5395513111175219e016e95f9db65e2738914826
Juliano <Juliano@DESKTOP-G33TORB.lan>
2020-05-25 20:43:03 -0300

    problemas resolvidos

--- Projeto_SegundoSemestre_PII/src/com/loja/informatica/VIEWS/telaVendedor.java
+++ Projeto_SegundoSemestre_PII/src/com/loja/informatica/VIEWS/telaVendedor.java
@@ -276,15 +276,13 @@ public class telaVendedor extends javax.swing.JFrame {
 
             },
             new String [] {
-                "Tipo", "ID", "Marca", "Quantidade", "Preço"
+                "Tipo", "Modelo", "Marca", "ID", "Preço", "Quantidade"
             }
         ));
-        tabelaCarrinho.getTableHeader().setReorderingAllowed(false);
         jScrollPane3.setViewportView(tabelaCarrinho);
 
         jLabel33.setText("Valor Final R$:");
 
-        txtValorFinal.setEnabled(false);
         txtValorFinal.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 txtValorFinalActionPerformed(evt);
@@ -312,9 +310,6 @@ public class telaVendedor extends javax.swing.JFrame {
                 txtCarrinhoQuantidadeActionPerformed(evt);
             }
         });
-
-        txtCarrinhoID.setEnabled(false);
-        txtCarrinhoID.setFocusable(false);
 
         jLabel32.setText("Quantidade");
 
@@ -406,7 +401,7 @@ public class telaVendedor extends javax.swing.JFrame {
                         .addComponent(jLabel30)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(txtCpfCompra, javax.swing.GroupLayout.PREFERRED_SIZE, 215, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addGap(18, 18, 18)
                         .addComponent(btIniciarVenda)
                         .addGap(18, 18, 18)
                         .addComponent(btCancelarVenda)
