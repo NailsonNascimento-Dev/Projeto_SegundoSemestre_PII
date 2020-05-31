1ff3491d3bcd9f37fbdc0c9d67c49ea7b5cc02b3
Marcos Dead <Marcos.viniciusk8@hotmail.com>
2020-05-31 14:39:21 -0300

    Desenvolvido classe controller dos relatorios

---
+++ Projeto_SegundoSemestre_PII/src/com/loja/informatica/CONTROLLER/ControllerRelatorios.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.loja.informatica.CONTROLLER;
+
+import com.loja.informatica.DAO.RelatoriosDAO;
+import com.loja.informatica.MODEL.Relatorios;
+import java.util.ArrayList;
+import java.util.Date;
+
+/**
+ *
+ * @author Marcos Vinícius Santos Souza
+ */
+public class ControllerRelatorios {
+
+    public static ArrayList<String[]> BuscarRegistros(Date dataInicio, Date dataFim) {
+
+        ArrayList<Relatorios> listarRegistros = new ArrayList<>();
+
+        ArrayList<String[]> retorno = new ArrayList<>();
+
+        listarRegistros = RelatoriosDAO.gerarRelatorio(dataInicio, dataFim);
+
+        for (Relatorios relatorios : listarRegistros) {
+            retorno.add(new String[]{
+                String.valueOf(relatorios.getIdVenda()),
+                String.valueOf(relatorios.getNome()),
+                String.valueOf(relatorios.getCpf()),
+                String.valueOf(relatorios.getData()),
+                String.valueOf(relatorios.getValorTotal())
+
+            });
+        }
+
+        return retorno;
+
+    }
+}
