beab3a972568a82e179e3e7d8313afd40a109cd9
Marcos Dead <Marcos.viniciusk8@hotmail.com>
2020-05-31 20:33:12 -0300

    Desenvolvido controller da descrição da venda

--- Projeto_SegundoSemestre_PII/src/com/loja/informatica/CONTROLLER/ControllerRelatorios.java
+++ Projeto_SegundoSemestre_PII/src/com/loja/informatica/CONTROLLER/ControllerRelatorios.java
@@ -38,4 +38,24 @@ public class ControllerRelatorios {
         return retorno;
 
     }
+    
+    public static ArrayList<String[]> buscarRegistroDetalhe(int idVenda){
+        
+        ArrayList<Relatorios> listarRegistros = new ArrayList<>();
+        
+        ArrayList<String[]> retorno = new ArrayList<>();
+        
+        listarRegistros = RelatoriosDAO.gerarRelatorio2(idVenda);
+        
+        for (Relatorios relatorios : listarRegistros) {
+            retorno.add(new String[]{
+                String.valueOf(relatorios.getTipoModelo()),
+                String.valueOf(relatorios.getDescricao()),
+                String.valueOf(relatorios.getQuantidade()),
+                String.valueOf(relatorios.getValorUnitario())
+            });
+        }
+        
+        return retorno;
+    }
 }
