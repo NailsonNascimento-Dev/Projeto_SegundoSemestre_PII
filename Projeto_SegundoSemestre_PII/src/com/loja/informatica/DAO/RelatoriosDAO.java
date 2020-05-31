af97f4b71d5e1b8c4e9c195097ebfd22a8dae3c9
Marcos Dead <Marcos.viniciusk8@hotmail.com>
2020-05-31 14:40:09 -0300

    Desenvolvido classe RelatorioDAO que vai se conectar com o banco de dados para trazer os registros

---
+++ Projeto_SegundoSemestre_PII/src/com/loja/informatica/DAO/RelatoriosDAO.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.loja.informatica.DAO;
+
+import com.loja.informatica.MODEL.Relatorios;
+import com.loja.informatica.UTILS.ConexaoMysql;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Date;
+
+/**
+ *
+ * @author Azazel
+ */
+public class RelatoriosDAO {
+
+    public static ArrayList<Relatorios> gerarRelatorio(Date dataInicio, Date dataFim){
+        ResultSet resultado = null;
+        Connection conexao = null;
+        PreparedStatement instrucaoSQL = null;
+        
+        ArrayList<Relatorios> listarRegistros = new ArrayList<>();
+        
+        try {
+            conexao = ConexaoMysql.abrirConexao();
+            
+            instrucaoSQL = conexao.prepareStatement("Select v.id_venda, c.nome, v.cpf, v.data_hoje, v.valor_total from venda as v inner join cliente as c on c.cpf = v.cpf where date(data_hoje) >= ? and date(data_hoje) <= ?;");
+            
+            instrucaoSQL.setDate(1, (java.sql.Date)dataInicio);
+            instrucaoSQL.setDate(2, (java.sql.Date)dataFim);
+            
+            resultado = instrucaoSQL.executeQuery();
+            
+            while(resultado.next()){
+                Relatorios relatorios = new Relatorios();
+                
+                relatorios.setIdVenda(resultado.getInt("id"));
+                relatorios.setNome(resultado.getString("nome"));
+                relatorios.setCpf(resultado.getString("cpf"));
+                relatorios.setData(resultado.getDate("data_hoje"));
+                relatorios.setValorTotal(resultado.getDouble("valor_total"));
+                
+                listarRegistros.add(relatorios);
+                
+                
+                
+            }
+            
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+            listarRegistros = null;
+        } finally {
+
+            try {
+                if (resultado != null) {
+                    resultado.close();
+                }
+                if (instrucaoSQL != null) {
+                    instrucaoSQL.close();
+                }
+
+                ConexaoMysql.fecharConexao();
+
+            } catch (SQLException ex) {
+            }
+        }
+
+        return listarRegistros;
+
+    }
+     
+}
