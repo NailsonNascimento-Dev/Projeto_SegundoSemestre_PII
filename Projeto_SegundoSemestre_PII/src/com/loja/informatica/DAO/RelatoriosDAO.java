5df3480988e1b169b1ca72518db12bf7dd432dd0
Marcos Dead <Marcos.viniciusk8@hotmail.com>
2020-05-31 20:26:30 -0300

    Metodo para o relatorio com os detalhes da venda

--- Projeto_SegundoSemestre_PII/src/com/loja/informatica/DAO/RelatoriosDAO.java
+++ Projeto_SegundoSemestre_PII/src/com/loja/informatica/DAO/RelatoriosDAO.java
@@ -20,38 +20,36 @@ import java.util.Date;
  */
 public class RelatoriosDAO {
 
-    public static ArrayList<Relatorios> gerarRelatorio(String dataInicio, String dataFim){
+    public static ArrayList<Relatorios> gerarRelatorio(String dataInicio, String dataFim) {
         ResultSet resultado = null;
         Connection conexao = null;
         PreparedStatement instrucaoSQL = null;
-        
+
         ArrayList<Relatorios> listarRegistros = new ArrayList<>();
-        
+
         try {
             conexao = ConexaoMysql.abrirConexao();
-            
+
             instrucaoSQL = conexao.prepareStatement("Select v.id_venda, c.nome, v.cpf, v.data_hoje, v.valor_total from venda as v inner join cliente as c on c.cpf = v.cpf where date(data_hoje) >= ? and date(data_hoje) <= ?;");
-            
+
             instrucaoSQL.setString(1, dataInicio);
             instrucaoSQL.setString(2, dataFim);
-            
+
             resultado = instrucaoSQL.executeQuery();
-            
-            while(resultado.next()){
+
+            while (resultado.next()) {
                 Relatorios relatorios = new Relatorios();
-                
+
                 relatorios.setIdVenda(resultado.getInt("id_venda"));
                 relatorios.setNome(resultado.getString("nome"));
                 relatorios.setCpf(resultado.getString("cpf"));
                 relatorios.setData(resultado.getDate("data_hoje"));
                 relatorios.setValorTotal(resultado.getDouble("valor_total"));
-                
+
                 listarRegistros.add(relatorios);
-                
-                
-                
+
             }
-            
+
         } catch (SQLException ex) {
             System.out.println(ex.getMessage());
             listarRegistros = null;
@@ -74,5 +72,55 @@ public class RelatoriosDAO {
         return listarRegistros;
 
     }
-     
+
+    public static ArrayList<Relatorios> gerarRelatorio2(int idVenda) {
+        ResultSet resultado = null;
+        Connection conexao = null;
+        PreparedStatement instrucaoSQL = null;
+
+        ArrayList<Relatorios> listarRegistros = new ArrayList<>();
+
+        try {
+            conexao = ConexaoMysql.abrirConexao();
+
+            instrucaoSQL = conexao.prepareStatement(" select tp.modelo_tipo, p.descricao, d.quantidade, p.preco from tipo_produto as tp inner join produto as p on p.id_tipo = tp.id_tipo inner join detalhes as d on d.modelo_codigo = p.modelo_codigo inner join venda as v where v.id_venda = ?;");
+
+            instrucaoSQL.setInt(1, idVenda);
+
+            resultado = instrucaoSQL.executeQuery();
+
+            while (resultado.next()) {
+                Relatorios relatorios = new Relatorios();
+
+                relatorios.setTipoModelo(resultado.getString("modelo_tipo"));
+                relatorios.setDescricao(resultado.getString("descricao"));
+                relatorios.setQuantidade(resultado.getInt("quantidade"));
+                relatorios.setValorUnitario(resultado.getDouble("preco"));
+
+                listarRegistros.add(relatorios);
+
+            }
+
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+            listarRegistros = null;
+        } finally {
+
+            try {
+                if (resultado != null) {
+                    resultado.close();
+                }
+                if (instrucaoSQL != null) {
+                    instrucaoSQL.close();
+                }
+
+                ConexaoMysql.fecharConexao();
+
+            } catch (SQLException ex) {
+            }
+        }
+
+        return listarRegistros;
+    }
+
 }
