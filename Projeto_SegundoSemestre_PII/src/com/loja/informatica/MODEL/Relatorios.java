d28ce441605d22599ee8ca3491ef54391c4efd1e
Marcos Dead <Marcos.viniciusk8@hotmail.com>
2020-05-31 14:40:48 -0300

    Desenvolvido classe Relatorio que contem os atributos que serão usados na hora de trazer os registros.

---
+++ Projeto_SegundoSemestre_PII/src/com/loja/informatica/MODEL/Relatorios.java
@@ -0,0 +1,101 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.loja.informatica.MODEL;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Marcos Vinícius Santos Souza
+ */
+public class Relatorios {
+    
+    private int idVenda;
+    private String nome;
+    private String cpf;
+    private Date data;
+    private double valorTotal;
+    
+    private String tipoModelo;
+    private String descricao;
+    private int quantidade;
+    private double valorUnitario;
+
+    public int getIdVenda() {
+        return idVenda;
+    }
+
+    public void setIdVenda(int idVenda) {
+        this.idVenda = idVenda;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+
+    public double getValorTotal() {
+        return valorTotal;
+    }
+
+    public void setValorTotal(double valorTotal) {
+        this.valorTotal = valorTotal;
+    }
+
+    public String getTipoModelo() {
+        return tipoModelo;
+    }
+
+    public void setTipoModelo(String tipoModelo) {
+        this.tipoModelo = tipoModelo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public double getValorUnitario() {
+        return valorUnitario;
+    }
+
+    public void setValorUnitario(double valorUnitario) {
+        this.valorUnitario = valorUnitario;
+    }
+    
+    
+    
+}
