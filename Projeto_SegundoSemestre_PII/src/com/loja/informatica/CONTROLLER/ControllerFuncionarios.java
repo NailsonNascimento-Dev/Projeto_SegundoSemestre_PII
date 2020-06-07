51339f54c657dbc19db6df8c063616a6011ad78e
Marcos Dead <Marcos.viniciusk8@hotmail.com>
2020-06-07 11:41:20 -0300

    Gerado javadoc da classe controller Funcionarios

--- Projeto_SegundoSemestre_PII/src/com/loja/informatica/CONTROLLER/ControllerFuncionarios.java
+++ Projeto_SegundoSemestre_PII/src/com/loja/informatica/CONTROLLER/ControllerFuncionarios.java
@@ -11,10 +11,30 @@ import java.util.ArrayList;
 
 /**
  *
- * @author Azazel
+ * @author Marcos Vinícius Santos Souza
+ *
  */
 public class ControllerFuncionarios {
 
+    /**
+     * Metodo desenvolvido para passar a requisição da view para a DAO pondendo
+     * cadastrar um registro
+     *
+     * @param nome
+     * @param sexo
+     * @param data
+     * @param cpf
+     * @param cargo
+     * @param rua
+     * @param cep
+     * @param numeroCasa
+     * @param bairro
+     * @param email
+     * @param telefone
+     * @param senha
+     * @return retorna true caso a os dados passados consigam ser inseridos no
+     * banco de dados atraves da classe funcionarioDAo
+     */
     public static boolean CadastrarFuncionario(String nome, String sexo, String data, String cpf, String cargo, String rua,
             String cep, int numeroCasa, String bairro, String email, String telefone, String senha) {
 
@@ -36,6 +56,26 @@ public class ControllerFuncionarios {
         return FuncionariosDAO.cadastrarFuncionario(funcionarios);
     }
 
+    /**
+     * Metodo desenvolvido para alterar um registro enviando a requisisão da
+     * view para a classe DAO
+     *
+     * @param id
+     * @param nome
+     * @param sexo
+     * @param data
+     * @param cpf
+     * @param cargo
+     * @param rua
+     * @param cep
+     * @param numeroCasa
+     * @param bairro
+     * @param email
+     * @param telefone
+     * @param senha
+     * @return retorna true caso os dados passados para a classe funcionarioDAO
+     * consigam consigam atualizar determinado registro no banco de dados
+     */
     public static boolean AlterarRegistro(int id, String nome, String sexo, String data, String cpf, String cargo, String rua,
             String cep, int numeroCasa, String bairro, String email, String telefone, String senha) {
 
@@ -57,11 +97,27 @@ public class ControllerFuncionarios {
         return FuncionariosDAO.atualizarRegistro(funcionarios);
     }
 
+    /**
+     * Metodo desenvolvido para enviar a exclusão de um registro pelo seu id
+     * para a classe DAO
+     *
+     * @param id
+     * @return retorna true caso consiga excluir um registro atraves da classe
+     * DAO
+     */
     public static boolean ExcluirRegistro(int id) {
 
         return FuncionariosDAO.excluirRegistro(id);
     }
 
+    /**
+     * Metodo desenvolvido para enviar a requisição da view para a DAO para
+     * buscar um registro em especifico
+     *
+     * @param busca
+     * @return retorna um array de string contendo os dados do rigistro em
+     * especifico a ser buscado
+     */
     public static ArrayList<String[]> BuscarRegistro(String busca) {
         ArrayList<Funcionarios> listarRegistros = new ArrayList<>();
 
@@ -91,6 +147,13 @@ public class ControllerFuncionarios {
 
     }
 
+    /**
+     * Metodo desenvolvido para enviar a requisição da viw para a classe DAO
+     * trazendo todos os registros da tabela
+     *
+     * @return retorna um array de string contendo todos os dados da tabela do
+     * banco de dados
+     */
     public static ArrayList<String[]> CarregarRegistros() {
 
         ArrayList<Funcionarios> listarRegistros = new ArrayList<>();
@@ -119,18 +182,19 @@ public class ControllerFuncionarios {
 
         return retorno;
     }
-    
-    
-    
-    public static String login (String senha1, int id){
-        
+/**
+ * Metodo desenvolvido para enviar a requisição de login da view para classe DAO para o usuario poder se logar no sistema
+ * @param senha1
+ * @param id
+ * @return retorna uma string contendo o cargo em especifico 
+ */
+    public static String login(String senha1, int id) {
+
         String cargo;
-        
         Funcionarios usuario = new Funcionarios();
-        
-       usuario.setSenha1(senha1);
-       usuario.setId(id);
-       cargo = FuncionariosDAO.telaLogin(usuario);
-      return cargo;
+        usuario.setSenha1(senha1);
+        usuario.setId(id);
+        cargo = FuncionariosDAO.telaLogin(usuario);
+        return cargo;
     }
 }
